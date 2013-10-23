979c887398128a4dcb338ab086938f53ba6b4b5b
Joesus <joesusnick@gmail.com>
2013-10-23 09:33:21 -0600

    Week Two Homework

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,10 @@ CodingNotes::Application.routes.draw do
 
 
   root 'static_pages#home'
-  match '/index', to: "static_pages#index", via: 'get'
-  match '/style', to: "static_pages#style", via: 'get'
-  match '/ruby',  to: "static_pages#ruby",  via: 'get'
-  match '/js',    to: "static_pages#js",    via: 'get'
+  match '/index',      to: "static_pages#index",      via: 'get'
+  match '/about',      to: "static_pages#about",      via: 'get'
+  match '/portfolio',  to: "static_pages#portfolio",  via: 'get'
+  match '/contact',    to: "static_pages#contact",    via: 'get'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
